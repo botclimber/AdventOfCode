a2dcce2cb615231beaea1460c56cc3df3a9244ef
uie64386 <daniel.filipe.silva@conti.de>
2022-12-12 17:20:08 +0000

    some initial setup

--- Day-12/res.js
+++ Day-12/res.js
@@ -2,6 +2,48 @@
 
 /**
 
+	S current position
+	E location with best signal
 	
+	going from a to z before reaching E 
+	
+	NOTE: use few steps as possible
 
+	APPROACH:
+		- matrix ?
+		- run thro all indexes following the alphabetic condition each time we end up in a no escape state we start from the begining ? or we can persist some path
+		
+			- e.g.
+				a b c
+				b b d
+				b a e
+				
+				- we can persist all positions that have more than one way to be chosen
+	
 */
+const data = document.getElementsByTagName("pre")[0].textContent.split("\n")
+
+var dayTwelve = (function(){
+	
+	"use strict";
+	function getData(){
+		var j = []
+		for (let i = 0; i < data.length -1 ; i++) j.push([...data[i]])
+		
+		return j
+	}
+	
+	const heightMap =getData()
+
+		
+	
+	// computation goes here
+	
+	
+	// ---------
+	
+	return {heightMap}
+	
+}())
+
+console.log(dayTwelve.heightMap)
